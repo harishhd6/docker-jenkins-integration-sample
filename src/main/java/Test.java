bdabf74766480720b42e431a947a0dde41decee3
Ubuntu <ubuntu@ip-172-31-15-141.ap-south-1.compute.internal>
2023-12-08 09:21:15 +0000

    deleted test

--- src/main/java/Test.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-   public static void main(String[] args) {
-       System.out.println(getMessage());
-   }
-   public static String getMessage() {
-       return "Hello, World!";
-   }
-}
