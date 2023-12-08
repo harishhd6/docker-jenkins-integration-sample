d371d4a256db6c863161addc95709f74c22a325c
Ubuntu <ubuntu@ip-172-31-15-141.ap-south-1.compute.internal>
2023-12-08 09:17:48 +0000

    new hello

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+       System.out.println(getMessage());
+   }
+   public static String getMessage() {
+       return "Hello, World!";
+   }
+}
